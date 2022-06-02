61495bafe363e191e4de3706549282db54f226d3
codedgame <106733600+codedgame@users.noreply.github.com>
2022-06-02 13:47:50 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,62 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function( m = matrix() ) {
 
+   #setthe inverse property	
+    i <- NULL
+
+  #set the matrix
+    set <- function( matrix ) {
+            m <<- matrix
+            i <<- NULL
+    }
+
+   
+    get <- function() {
+    	## Return the matrix
+    	m
+    }
+
+   #set the inverse of the matrix
+    setInverse <- function(inverse) {
+        i <<- inverse
+    }
+
+    # get the inverse of the matrix
+    getInverse <- function() {
+       
+        i
+    }
+
+    # print a list of the methods
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+   
+    m <- x$getInverse()
+
+    
+    if( !is.null(m) ) {
+            message("getting cached data")
+            return(m)
+    }
+
+  
+    data <- x$get()
+
+   
+    m <- solve(data) %*% data
+
+    #  inverse to the object
+    x$setInverse(m)
+
+   
+    m
 }
